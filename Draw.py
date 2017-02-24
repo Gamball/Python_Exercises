c62974419b1233c9df43e133586f365f559ede6d
Omar Abdullah <maitreomar25@yahoo.com>
2017-02-24 08:53:52 -1200

    Draw
    the function for this program is drawing simple shapes...
    Be Happy :)

---
+++ Draw.py
@@ -0,0 +1,8 @@
+def main():
+    x = int(input('x =\t'))
+    y = int(input('y =\t'))
+    print('\n')
+    for i in range(y):
+        print('*' * x)
+    print('\n')
+if __name__ == '__main__': main()

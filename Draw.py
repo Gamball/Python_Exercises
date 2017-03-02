96f0bfa6f42ca09882441151149737dfd38472ea
Gumball-جامبول <maitreomar25@yahoo.com>
2017-03-01 12:02:06 -1200

    Draw Square OR Rectangle

--- Draw.py
+++
@@ -1,8 +0,0 @@
-def main():
-    x = int(input('x =\t'))
-    y = int(input('y =\t'))
-    print('\n')
-    for i in range(y):
-        print('*' * x)
-    print('\n')
-if __name__ == '__main__': main()
---
+++ Square_OR_Rectangle.py
@@ -0,0 +1,12 @@
+
+""" Draw Square OR Rectangle """
+
+
+def main():
+    x = int(input('x =\t'))
+    y = int(input('y =\t'))
+    for i in range(y):
+        print('*' * x)
+    print('you are draw this shape (:\n')
+if __name__ == '__main__':
+        main()

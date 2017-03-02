ee00e9344ea0c2eb3120df0f0a1e7e5750a44a65
Gumball-جامبول <maitreomar25@yahoo.com>
2017-03-01 12:00:39 -1200

    Draw Tree

--- tree.py
+++ tree.py
@@ -1,14 +1,20 @@
 
 
-def main():
-    a = list(range(1, 6))
-    a.sort(reverse=True)
-    for o in a:
-        print(o)
+""" Draw Tree """
 
-    for z in range(1, 6):
-        a = list(range(1, 6))
-        a.sort(reverse=True)
-        print(z * '*', * '*')
+
+def main():
+    # Ask the user about how number of the row:
+    rows = int(input('inter No. of rows:'))
+
+    # # The Roll is => { i * 2 - 1 } & { rows - i}
+    for i in range(rows):
+        print((rows - i) * ' ', (i * 2 - 1) * '*')
+
+    for i in range(rows // 2, rows):
+        print((rows - i) * ' ', (i * 2 - 1) * '*')
+
+    for i in range(3):
+        print((rows - 2) * ' ', '||')
 if __name__ == '__main__':
     main()

278822dfa5db0e9763012c847b37b5b4bb85be27
Gumball-جامبول <maitreomar25@yahoo.com>
2018-02-20 21:14:55 +0200

    Update Pyramid.py

--- Pyramid.py
+++ Pyramid.py
@@ -4,7 +4,7 @@
 
 
 def main():
-    # Ask the user about how number of the row:
+    # Ask the user about how number of the rows:
     rows = int(input('inter No. of rows:'))
 
     # # The Roll is => { i * 2 - 1 } & { rows - i}
